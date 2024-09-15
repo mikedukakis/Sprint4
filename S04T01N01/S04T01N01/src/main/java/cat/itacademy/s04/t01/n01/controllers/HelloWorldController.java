109e3560c3c4a48d84d3df8964d5e535f513add6
Ignasi Moya <imoyaf@gmail.com>
2024-09-15 12:44:19 +0200

    First commit

---
+++ S04T01N01/S04T01N01/src/main/java/cat/itacademy/s04/t01/n01/controllers/HelloWorldController.java
@@ -0,0 +1,23 @@
+package cat.itacademy.s04.t01.n01.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/HelloWorld")
+    public String greets(@RequestParam(defaultValue = "UNKNOWN") String name) {
+        return "Hello, " + name + ". You are running a Maven project";
+    }
+
+    @GetMapping(value = {"/HelloWorld2", "/HelloWorld2/{name}"})
+    public String greets2(@PathVariable(required = false) String name) {
+        if (name == null) {
+            name = "UNKNOWN";
+        }
+        return "Hello, " + name + ". You are running a Maven project";
+    }
+}
